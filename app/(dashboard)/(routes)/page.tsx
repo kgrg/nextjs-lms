af2b7e594327d2c72f109ed5cad84b572feec60f
kgurungsein <kgurung@sein.tech>
2024-01-14 00:41:42 +0000

    feat(setup) : next project setup for layout

---
+++ app/(dashboard)/(routes)/page.tsx
@@ -0,0 +1,11 @@
+const dashboard = () => {
+    return (
+        <div className="h-full">
+            <div className="hidden md:flex">
+                <h1>Dashboard</h1>
+            </div>
+        </div>
+    );
+}
+
+export default dashboard; 
