296ce785d5f8603387a314b3b9665951084ee6e3
kgurungsein <kgurung@sein.tech>
2024-01-15 22:44:37 +0000

    feat(routing): route navigation teacher mode

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,14 +1,13 @@
 "use client"
 
 import { UserButton } from "@clerk/nextjs";
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 import { Button } from "./ui/button";
 import { LogOut } from "lucide-react";
 import Link from "next/link";
 
 export const NavbarRoutes = () => {
     const pathname = usePathname();
-    const router = useRouter();
 
     const isTeacherPage = pathname?.startsWith("/teacher");
     const isPlayerPage = pathname?.startsWith("/player");
@@ -16,10 +15,12 @@ export const NavbarRoutes = () => {
     return (
         <div className="flex gap-x-2 ml-auto">
             {isTeacherPage || isPlayerPage ? (
-                <Button>
-                    <LogOut className="h-4 w-4 mr-4" />
-                    Exit
-                </Button>
+                <Link href="/">
+                    <Button size="sm" variant="ghost">
+                        <LogOut className="h-4 w-4 mr-4" />
+                        Exit
+                    </Button>
+                </Link>
             ) : (
                 <Link href="/teacher/courses">
                     <Button size="sm" variant="ghost">
